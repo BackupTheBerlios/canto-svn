9f1bfb88449b6c0d57da07778200ce6ed9a81e24
neoraph <neoraph@be5e2811-002a-0410-8f54-c5534af08d37>
2007-03-06 14:37:47 +0000

    add dao interface definition

---
+++ trunk/modules/rajenlab-common/source/java/li/rajenlab/common/dao/EntityNotFoundException.java
@@ -0,0 +1,35 @@
+/****************************************************************************** 
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ * $LastChangedDate$
+ * 
+ * 
+ ******************************************************************************
+ * Project: rajenlab-common
+ ******************************************************************************
+ * $HeadURL$
+ ******************************************************************************/
+
+package li.rajenlab.common.dao;
+
+public class EntityNotFoundException extends Exception {
+
+	private static final long serialVersionUID = -5897460678391866399L;
+
+	public EntityNotFoundException() {
+		super();
+	}
+
+	public EntityNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+
+	public EntityNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+}
